1b6a539663f333a36bdbe6ab8fb555dfff6b7573
turbine1449 <aicreative2020.041@gmail.com>
2020-06-30 22:39:19 +0900

    Pascal

--- test.c
+++ test.c
@@ -1,8 +1,32 @@
 #include <stdio.h>
+#define N 12
 
-int main(){
+/* プロトタイプ宣言　*/
+long combi(int, int);
 
-    printf("Hello World\n");
+void main(){
 
-    return 0;
+    int n, r, t ;
+
+    for(n = 0; n <= N; n++){
+        for(t = 0; t < (N-n)*3; t++){
+            printf(" ");
+        }
+        for(r = 0; r <= n; r++){
+            printf("%3ld", combi(n,r));
+        }
+        printf("\n\n");
+    }
+    
+}
+
+long combi(int n, int r){
+
+    int i;
+    long p = 1;
+
+    for(i = 1; i <= r; i++){
+        p = p*(n-i+1) / i;
+    }
+    return p;
 }
