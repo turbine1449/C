875aa57b13a338105a00a8e22f9fa6488a36d529
turbine1449 <aicreative2020.041@gmail.com>
2020-07-15 22:56:52 +0900

    07/15 C言語学習

--- chapter06.c
+++ chapter06.c
@@ -1,6 +1,16 @@
 #include <stdio.h>
 
 int main(void){
+    int price;
+    
+    printf("金額を入力してください。\n");
+
+    scanf("%d",&price);
+
+    printf("1割引は%d円です。\n",(int)(price * 0.9));
+    printf("3割引は%d円です。\n",(int)(price * 0.7));
+    printf("5割引は%d円です。\n",(int)(price * 0.5));
+    printf("8割引は%d円です。\n",(int)(price * 0.2));
 
    
    
