adf95a7f8a2a43e197a11c1d2ace3fa971d432f7
turbine1449 <aicreative2020.041@gmail.com>
2020-07-24 22:35:47 +0900

    07/24 C言語学習

--- chapter08.c
+++ chapter08.c
@@ -1,6 +1,61 @@
 #include <stdio.h>
 
 int main(void){
+
+    int no;
+    scanf("%d",&no);
+    switch (no){
+        case 1:
+        case 3:
+        case 4:
+            printf("男性\n");
+            break;
+        case 2:
+            printf("女性\n");
+            break;
+        default:
+            printf("Unknown");
+            break;
+    }
+
+/*
+    int no;
+    scanf("%d",&no);
+    switch (no){
+        case 1:
+            printf("野比のび太\n");
+            break;
+        case 2:
+            printf("源静香\n");
+            break;
+        case 3:
+            printf("剛田武\n");
+            break;
+        case 4:
+            printf("骨川スネ夫\n");
+            break;
+        default:
+            printf("Unknown\n");
+            break;
+    }
+
+
+/*
+    int no;
+    scanf("%d",&no);
+    if (no == 1){
+        printf("野比のび太\n");
+    } else if (no == 2){
+        printf("源静香\n");
+    } else if (no == 3){
+        printf("剛田武\n");
+    } else if (no == 4){
+        printf("骨川スネ夫\n");
+    } else {
+        printf("Unknown\n");
+    }
+
+/*
     int age;
     printf("年齢：");
     scanf("%d",&age);
@@ -70,5 +125,22 @@ if (条件){
 }
 ---------------
 ↑結局この書き方が見やすいので、普段からこう書こう。
+
+
+
+番号による場合分け
+switch 文～caseを使うと美しい。
+switch文は、指定された条件式の値と同じ値のcaseへジャンプする。
+ジャンプ先ではcase以下の文を実行し、break(ブレイク)文を見つけたら、
+switch文の後で囲っている {} の中から抜け出す。
+当てはまらなかった場合の処理はdefaultを使用して記述する。
+
+caseは、複数を連続させて使うことが可能。
+case自体には、switch文でジャンプする場所を示す意味しか無いため、
+caseを複数つなげても、実行内容には影響がない。
+caseの条件として使えるのは整数値のみ。
+つまり、switch文で指定した変数とcaseの整数値の比較しかできない。
+変数や実数、条件式は使えないため、判断力は弱い。
+
 
 */
