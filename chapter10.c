6168f5c409b3ceb34e386f917e30b3c869263086
turbine1449 <aicreative2020.041@gmail.com>
2020-08-03 22:38:23 +0900

    08/03 C言語学習

--- chapter10.c
+++ chapter10.c
@@ -1,6 +1,57 @@
 #include <stdio.h>
 
 int main (void){
+
+    int r;
+    double s;
+
+    do {
+        printf("半径？：");
+        scanf("%d",&r);
+    }while (r < 0);
+
+    s = r * r * 3.14;
+    printf("面積は%fです。\n",s);
+
+
+/*
+※再入力はできているが、同じデータ入力の処理を２回書いているのが無駄
+    int r;
+    double s;
+
+    printf("半径？：");
+    scanf("%d",&r);
+
+    while (r < 0){
+        printf("半径？：");
+        scanf("%d",&r);
+    }
+
+    s = r * r * 3.14;
+    printf("面積は%fです。\n",s);
+
+
+
+
+/*
+※条件不一致の場合に再入力できない。
+    int r;
+    double s;
+
+    printf("半径?:");
+    scanf("%d",&r);
+    if (r < 0){
+        printf("半径は負の数になりません。");
+
+    }else{
+        s = r * r * 3.14;
+        printf("面積は%fです。\n",s);
+
+    }
+    
+
+
+/*
     double money = 1;
     int month = 1;
 
@@ -10,7 +61,7 @@ int main (void){
         month++;
     }
     printf("%02dヶ月目に%7.0f円となり、100万円を超える。\n",month,money);
-
+*/
 
 
 
@@ -28,5 +79,17 @@ while (条件式) {
 繰り返す文;
 }
 -------
+
+
+while文は、繰り返す文の実行を行う前に条件式を判定する。
+しかし、do～while文は、繰り返す文の実行後に条件式を判定する。
+-------
+//構文
+do {
+繰り返す文;
+} while (条件式);
+-------
+do～while文には、1つ大きな利点がある。
+それは、繰り返し文が、必ず1回は実行されるという点だ。
 
 */
